2ec72e85aa755a635a328960a7706a5cbe93ebdc
dimmo <dimmoborgir@gmail.com>
2013-04-16 02:09:08 +0600

    DB Backup script

---
+++ checksystem/backup-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DBNAME=ructfetest
+OUTDIR=/home/user/backup
+
+FNAME=$OUTDIR/`date +"%Y-%m-%d-%H%M%S".sql.gz`
+echo -n "Making backup of '$DBNAME' to '$FNAME' ... "
+/usr/bin/pg_dump $DBNAME | gzip -c > $FNAME
+echo "done: "`stat -c %s $FNAME`" bytes"
+
