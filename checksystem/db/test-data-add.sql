ae3ab82a08f03731e5d492e14b39e20647e800a1
dimmo <dimmoborgir@gmail.com>
2013-04-17 03:25:26 +0600

    [checksystem] initial test data: add SES checker, remove Rand checker

--- checksystem/db/test-data-add.sql
+++ checksystem/db/test-data-add.sql
@@ -5,12 +5,11 @@ INSERT INTO teams VALUES ( 102, 'Team 102', '172.16.16.102/32', 'team2.ructf', t
 INSERT INTO teams VALUES ( 103, 'Team 103', '172.16.16.103/32', 'team3.ructf', true );
 INSERT INTO teams VALUES ( 104, 'Team 104', '172.16.16.104/32', 'team4.ructf', true );
 
-INSERT INTO services VALUES ( 1, 'SES',       './stub/notexist.sh' );
+INSERT INTO services VALUES ( 1, 'SES',       './ses.checker.pl' );
 INSERT INTO services VALUES ( 2, 'MapReduce', './mr.checker.py'    );
 INSERT INTO services VALUES ( 3, 'MsgQueue',  './stub/notexist.sh' );
 INSERT INTO services VALUES ( 4, 'DNS',       './stub/notexist.sh' );
 INSERT INTO services VALUES ( 5, 'Balancer',  './stub/notexist.sh' );
 INSERT INTO services VALUES ( 6, 'DB',        './database.checker.py' );
 INSERT INTO services VALUES ( 7, 'ScriptAPI', './stub/notexist.sh' );
-INSERT INTO services VALUES ( 8, 'Rand',      './stub/random.pl'   );
 
