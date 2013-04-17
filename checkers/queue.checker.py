24a178b2ecae716ce95fbe602bdb4beb172c2897
Alexander Bersenev <bay@hackerdom.ru>
2013-04-17 22:12:08 +0600

    queue checker more verbose output

--- checkers/queue.checker.py
+++ checkers/queue.checker.py
@@ -55,8 +55,8 @@ def register_or_die(host, login, password):
     ans = requests.post("http://{0}/register".format(host),
                         data={"login": login, "first_name": login,
                               "last_name": "", "password": password,
-                              "language": "en"}, allow_redirects=True)
-    if ans.status_code != 200:
+                              "language": "en"})
+    if ans.status_code == 500:
         print("Failed to register the user in the user service: %s, %s, %s" % (login, host, ans.status_code))
         sys.exit(DOWN)
 
