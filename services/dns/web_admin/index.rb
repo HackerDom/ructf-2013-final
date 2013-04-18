9e60bba9c40bcdfc60ef4a9b7bcbf18abc4dbd3d
Len0k <len0k.matmex@gmail.com>
2013-04-18 21:12:11 +0600

    web should work now

--- services/dns/web_admin/index.rb
+++ services/dns/web_admin/index.rb
@@ -111,59 +111,71 @@ get %r{/(show)?} do
 end
 
 post '/' do
+  data = {}
+
   if request.media_type =~ /json/
     data = JSON.parse request.body.read
+  else
+    data['action'] = request["action"]
 
-    if request.cookies['session'] != nil
-      r_host = request.host
-      teamN = r_host[/team\d+/]
-      payload = {'session' => request.cookies['session']}.to_json
-      req = Net::HTTP::Post.new("/user/", initheader = {'X-Requested-With' => 'XMLHttpRequest', 'Content-Type' => 'application/json'})
-      req.body = payload
-      response = Net::HTTP.new("#{teamN}.ructf", 80).start {|http| http.request(req) }
-      r_hash = JSON.parse(response.body)
+    if data['action'] == "ADD"
+      data['type'] = request["type"]
+      data['name'] = request["name"]
+      data['value'] = request["value"]
+    elsif data['action'] == "DELETE"
+      data['id'] = request["id"]
+    end
+  end
 
-      if r_hash['status'] == 'OK'
-        r_user_name = r_hash['first_name'] + " " + r_hash['last_name']
-        uid = r_hash['uid']
+  h = {'code' => 'ERROR', 'why' => '42'}.to_json
 
-        if data['action'] != nil
-          act = data['action']
+  if request.cookies['session'] != nil
+    r_host = request.host
+    teamN = r_host[/team\d+/]
+    payload = {'session' => request.cookies['session']}.to_json
+    req = Net::HTTP::Post.new("/user/", initheader = {'X-Requested-With' => 'XMLHttpRequest', 'Content-Type' => 'application/json'})
+    req.body = payload
+    response = Net::HTTP.new("#{teamN}.ructf", 80).start {|http| http.request(req) }
+    r_hash = JSON.parse(response.body)
 
-          if act == "ADD"
+    if r_hash['status'] == 'OK'
+      r_user_name = r_hash['first_name'] + " " + r_hash['last_name']
+      uid = r_hash['uid']
 
-            if data['type'] != nil and data['name'] != nil and data['value'] != nil
-              type = dbh.escape_string(data['type'])
-              name = dbh.escape_string(data['name'])
-              name.sub!(/team\d+\.ructf$/, '')
-              value = dbh.escape_string(data['value'])
-              res = dbh.query("Select * from records where dtype = '#{type}' and dkey = '#{name}' and dvalue = '#{value}'")
-              
-              if res.num_rows == 0
-                id = md5(md5(Process.pid.to_s)+md5(Time.new.to_s))
-                dbh.query("insert into records (did, dtype, dkey, dvalue, dcreator) values ('#{id}', '#{type}', '#{name}', '#{value}', '#{uid}')")
-                h = {'code' => 'OK', 'id' => id}.to_json
-              else
-                h = {'code' => 'ERROR', 'why' => 'already_exists'}.to_json
-              end
+      if data['action'] != nil
+        act = data['action']
+
+        if act == "ADD"
+
+          if data['type'] != nil and data['name'] != nil and data['value'] != nil
+            type = dbh.escape_string(data['type'])
+            name = dbh.escape_string(data['name'])
+            name.sub!(/team\d+\.ructf$/, '')
+            value = dbh.escape_string(data['value'])
+            res = dbh.query("Select * from records where dtype = '#{type}' and dkey = '#{name}' and dvalue = '#{value}'")
+            
+            if res.num_rows == 0
+              id = md5(md5(Process.pid.to_s)+md5(Time.new.to_s))
+              dbh.query("insert into records (did, dtype, dkey, dvalue, dcreator) values ('#{id}', '#{type}', '#{name}', '#{value}', '#{uid}')")
+              h = {'code' => 'OK', 'id' => id}.to_json
             else
-              h = {'code' => 'ERROR', 'why' => '42'}.to_json
+              h = {'code' => 'ERROR', 'why' => 'already_exists'}.to_json
             end
+          else
+            h = {'code' => 'ERROR', 'why' => '42'}.to_json
+          end
 
-          elsif act == "DELETE"
+        elsif act == "DELETE"
 
-            if data['id'] != nil
-              id = dbh.escape_string(data['id'])
-              res = dbh.query("Select * from records where did = '#{id}'")
-              
-              if res.num_rows > 0
-                dbh.query("Delete from records where did = '#{id}'")
-                h = {'code' => 'OK'}.to_json
-              else
-                h = {'code' => 'ERROR', 'why' => 'id_not_found'}.to_json
-              end
+          if data['id'] != nil
+            id = dbh.escape_string(data['id'])
+            res = dbh.query("Select * from records where did = '#{id}'")
+            
+            if res.num_rows > 0
+              dbh.query("Delete from records where did = '#{id}'")
+              h = {'code' => 'OK'}.to_json
             else
-              h = {'code' => 'ERROR', 'why' => '42'}.to_json
+              h = {'code' => 'ERROR', 'why' => 'id_not_found'}.to_json
             end
           else
             h = {'code' => 'ERROR', 'why' => '42'}.to_json
@@ -172,15 +184,15 @@ post '/' do
           h = {'code' => 'ERROR', 'why' => '42'}.to_json
         end
       else
-        h = {'code' => 'ERROR', 'why' => 'prohibited'}.to_json
+        h = {'code' => 'ERROR', 'why' => '42'}.to_json
       end
     else
       h = {'code' => 'ERROR', 'why' => 'prohibited'}.to_json
     end
-    
-    content_type "application/json"
-    "#{h}"
   else
-    "only json supported for now!"
+    h = {'code' => 'ERROR', 'why' => 'prohibited'}.to_json
   end
+  
+  content_type "application/json"
+  "#{h}"
 end
