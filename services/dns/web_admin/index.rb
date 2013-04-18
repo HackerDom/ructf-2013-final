bee98dcd67ba353f335c982310de558673d5acf8
Len0k <len0k.matmex@gmail.com>
2013-04-18 15:18:57 +0600

    bugzz

--- services/dns/web_admin/index.rb
+++ services/dns/web_admin/index.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'net/http'
 require 'json'
 require 'digest/md5'
+require 'mysql'
 
 show_template = ""
 eval File.open('./templates/show.rb').read
@@ -25,63 +26,6 @@ r_authored = false
 r_has_records = false
 
 dbh = Mysql.real_connect("localhost", "dns", "default_password", "dns")
-
-get %r{/(show)?/} do
-  if request.cookies['session'] != nil
-    r_host = request.host
-    teamN = r_host[/team\d+/]
-    payload = {'session' => request.cookies['session']}.to_json
-    req = Net::HTTP::Post.new("/user/", initheader = {'X-Requested-With' => 'XMLHttpRequest', 'Content-Type' => 'application/json'})
-    req.body = payload
-    response = Net::HTTP.new("#{teamN}.ructf", 80).start {|http| http.request(req) }
-    r_hash = JSON.parse(response.body)
-
-    if r_hash['status'] != 'OK'
-      r_authored = false
-      r_has_records = false
-      r_dns_records = []
-      r_user_name = "Cheater"
-    else
-      r_authored = true
-      id = dbh.escape_string(r_hash['uid'])
-      r_has_records = false
-      r_dns_records = []
-      res = dbh.query("Select did, dtype, dkey, dvalue from records where dcreator = #{id})")
-      if res.num_rows > 0
-        r_has_records = true
-        res.each do |row|
-          r_dns_records.push([row[1] + " | " + row[2] + " -> " + row[3], row[0]])
-        end
-      end
-      r_user_name = r_hash['first_name'] + " " + r_hash['last_name'] + "!"
-    end
-  else
-    r_authored = false
-    r_has_records = false
-    r_dns_records = []
-    r_user_name = "Log in!"
-  end
-  message = ERB.new(show_template, nil, "%")
-  payload = message.result
-  "#{payload}"
-end
-
-get "/show:id" do
-  id = dbh.escape_string(params[:id])
-  r_has_records = false
-  r_dns_records = []
-  res = dbh.query("Select did, dtype, dkey, dvalue from records where dcreator = '#{id}')")
-  if res.num_rows > 0
-    r_has_records = true
-    res.each do |row|
-      r_dns_records.push([row[1] + " | " + row[2] + " -> " + row[3], row[0]])
-    end
-  end
-  r_authored = r_has_records
-  message = ERB.new(show_template, nil, "%")
-  payload = message.result
-  "#{payload}"
-end
 
 get '/add' do
   if request.cookies['session'] != nil
@@ -105,6 +49,63 @@ get '/add' do
     r_user_name = "Log in!"
   end
   message = ERB.new(add_template, nil, "%")
+  payload = message.result
+  "#{payload}"
+end
+
+get "/show:id" do
+  id = dbh.escape_string(params[:id])
+  r_has_records = false
+  r_dns_records = []
+  res = dbh.query("Select did, dtype, dkey, dvalue from records where dcreator = '#{id}'")
+  if res.num_rows > 0
+    r_has_records = true
+    res.each do |row|
+      r_dns_records.push([row[1] + " | " + row[2] + " -> " + row[3], row[0]])
+    end
+  end
+  r_authored = r_has_records
+  message = ERB.new(show_template, nil, "%")
+  payload = message.result
+  "#{payload}"
+end
+
+get %r{/(show)?} do
+  if request.cookies['session'] != nil
+    r_host = request.host
+    teamN = r_host[/team\d+/]
+    payload = {'session' => request.cookies['session']}.to_json
+    req = Net::HTTP::Post.new("/user/", initheader = {'X-Requested-With' => 'XMLHttpRequest', 'Content-Type' => 'application/json'})
+    req.body = payload
+    response = Net::HTTP.new("#{teamN}.ructf", 80).start {|http| http.request(req) }
+    r_hash = JSON.parse(response.body)
+
+    if r_hash['status'] != 'OK'
+      r_authored = false
+      r_has_records = false
+      r_dns_records = []
+      r_user_name = "Cheater"
+    else
+      r_authored = true
+      id = dbh.escape_string(r_hash['uid'])
+      r_has_records = false
+      r_dns_records = []
+      res = dbh.query("Select did, dtype, dkey, dvalue from records where dcreator = '#{id}'")
+      if res.num_rows > 0
+        r_has_records = true
+        res.each do |row|
+          r_dns_records.push([row[1] + " | " + row[2] + " -> " + row[3], row[0]])
+        end
+      end
+      r_user_name = r_hash['first_name'] + " " + r_hash['last_name'] + "!"
+    end
+  else
+    r_authored = false
+    r_has_records = false
+    r_dns_records = []
+    r_user_name = "Log in!"
+  end
+  message = ERB.new(show_template, nil, "%")
   payload = message.result
   "#{payload}"
 end
@@ -136,7 +137,7 @@ post '/' do
               name = dbh.escape_string(data['name'])
               name.sub!(/team\d+\.ructf$/, '')
               value = dbh.escape_string(data['value'])
-              res = dbh.query("Select * from records where dtype = '#{type}' and dkey = '#{name}' and dvalue = '#{value}')")
+              res = dbh.query("Select * from records where dtype = '#{type}' and dkey = '#{name}' and dvalue = '#{value}'")
               
               if res.num_rows == 0
                 id = md5(md5(Process.pid.to_s)+md5(Time.new.to_s))
