f0b6df8d00048fd6ed6d9190b479c6ff7c8407d5
kost <kost@hackerdom.ru>
2013-04-19 06:24:31 +0600

    proxy service rules matching added

--- services/proxy/proxy.c
+++ services/proxy/proxy.c
@@ -11,8 +11,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #define TMP_BUF_LEN 4096
 #define PROXY_BUF_LEN 65536
+
+#define RULES_BUF_LEN 65536
+#define RULE_MAX_LENGTH 128
+
+#define LOG_ENTRY_MAX_LEN 256
 
 #define CONNECT_TIMEOUT 3
 #define INACTIVITY_TIMEOUT 15
@@ -39,6 +45,9 @@ typedef struct conn_state_s {
 int dst_addr;
 int dst_port;
 
+FILE *rules_file;
+char all_rules[RULES_BUF_LEN];
+
 conn_state *
 alloc_conn_state(int client_fd, int server_fd)
 {
@@ -49,6 +58,8 @@ alloc_conn_state(int client_fd, int server_fd)
     memset(state, 0, sizeof(*state));
     state->client_fd = client_fd;
     state->server_fd = server_fd;
+
+    memcpy(state->rules_buf, all_rules, sizeof(all_rules));
 
     return state;
 }
@@ -117,9 +128,35 @@ connect_with_timeout(int addr, int port, int timeout_sec) {
     }
 }
 
+char *
+nstrstr(const char *in, const char *str, int n){
+    char c;
+    size_t len;
+
+    c = *str++;
+    if (!c)
+        return (char *) 0;
+
+    len = strnlen(str, n);
+    
+    do {
+        char sc;
+
+        do {
+            sc = *in++;
+            if (!sc)
+                return (char *) 0;
+        } while (sc != c);        
+    } while (strncmp(in, str, len) != 0);
+
+    return (char *) (in - 1);    
+}
+
+
 int
-process_read(int fd, char *buff, int already_read) {
+process_read(int fd, char *buff, int already_read, char * rules) {
     char tmp[TMP_BUF_LEN];
+    memset(tmp, 0 , sizeof(tmp));
 
     int can_read = PROXY_BUF_LEN - already_read;
     if (can_read <= 0)
@@ -129,6 +166,17 @@ process_read(int fd, char *buff, int already_read) {
 
     if (result > 0)
     {
+        int i;
+        for (i = 0; i < RULES_BUF_LEN / RULE_MAX_LENGTH; i++) {
+            if (nstrstr(tmp, rules + RULE_MAX_LENGTH * i, RULE_MAX_LENGTH) != 0)
+            {
+                char log_mes[LOG_ENTRY_MAX_LEN];
+                snprintf(log_mes, LOG_ENTRY_MAX_LEN, "alert! rule '%s' matched", rules + RULE_MAX_LENGTH * i);
+                fprintf(stderr, log_mes);
+                return -1;
+            }
+        }
+
         memcpy(buff + already_read, tmp, result);
         return result;
     }
@@ -154,9 +202,8 @@ process_write(int fd, char *buff, int already_read, int already_sent) {
 }
 
 void *
-thread_func(int *fd_ptr)
+thread_func(int client_socket)
 {
-    int client_socket = *fd_ptr;
     make_nonblocking(client_socket);
 
     int server_socket = connect_with_timeout(dst_addr, dst_port, CONNECT_TIMEOUT);
@@ -187,14 +234,12 @@ thread_func(int *fd_ptr)
             if (!server_closed) {
                 FD_SET(state->client_fd, &readset);
                 max_fd = max(max_fd, state->client_fd);
-            }
-                
+            }                
 
             if (state->buff_s2c_n_written != state->buff_s2c_n_read) {
                 FD_SET(state->client_fd, &writeset);
                 max_fd = max(max_fd, state->client_fd);
-            }
-                
+            }                
         }
         
         if (!server_closed) {
@@ -277,10 +322,9 @@ thread_func(int *fd_ptr)
             }
         }
 
-
         if (!client_closed && FD_ISSET(state->client_fd, &readset))
         {
-            int result = process_read(state->client_fd, state->buff_c2s, state->buff_c2s_n_read);
+            int result = process_read(state->client_fd, state->buff_c2s, state->buff_c2s_n_read, state->rules_buf);
             if (result >= 0)
                 state->buff_c2s_n_read += result;
             else
@@ -292,7 +336,7 @@ thread_func(int *fd_ptr)
 
         if (!server_closed && FD_ISSET(state->server_fd, &readset))
         {
-            int result = process_read(state->server_fd, state->buff_s2c, state->buff_s2c_n_read);
+            int result = process_read(state->server_fd, state->buff_s2c, state->buff_s2c_n_read, state->rules_buf);
             if (result >= 0)
                 state->buff_s2c_n_read += result;
             else
@@ -356,7 +400,7 @@ run(int listen_port)
                 perror("pthread_attr_setstacksize");
                 return;
             }
-            if (pthread_create(&thread, &tattr, thread_func, &fd) != 0)
+            if (pthread_create(&thread, &tattr, thread_func, fd) != 0)
             {
                 perror("pthread_create");
                 return;
@@ -366,13 +410,12 @@ run(int listen_port)
     }
 }
 
-
 int
 main(int argc, char **argv) {
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    if (argc != 4) {
-        fprintf(stdout, "Usage: %s <listen_port> <dst_addr> <dst_port>\n", argv[0]);
+    if (argc != 5) {
+        fprintf(stdout, "Usage: %s <listen_port> <dst_addr> <dst_port> <rules_file>\n", argv[0]);
         return 1;
     }
 
@@ -390,6 +433,49 @@ main(int argc, char **argv) {
 
     dst_port = atoi(argv[3]);
     if (dst_port <=0 || dst_port > 65535) {
+        fprintf(stderr, "Invalid dst_port '%s'\n", argv[3]);
+        return 1;
+    }
+
+    memset(all_rules, 0, sizeof(all_rules));
+
+    rules_file = fopen(argv[4], "r");
+
+    if (rules_file == NULL) {
+        fprintf(stderr, "Can't open file '%s' for reading: %s\n", argv[4], strerror(errno));
+        return 1;
+    }
+
+    fseek(rules_file, 0L, SEEK_END);
+    if (ftell(rules_file) > RULES_BUF_LEN) {
+        fprintf(stderr, "Too big rules file '%s'\n", argv[4]);
+        return 1;
+    }
+    fseek(rules_file, 0L, SEEK_SET);
+
+    int rule_num = 0;
+    char rule[RULE_MAX_LENGTH];
+    memset(rule, 0, sizeof(rule));
+    while(fgets(rule, sizeof(rule), rules_file) != NULL)
+    {
+        if (rule[sizeof(rule) - 1] != 0)
+        {
+            fprintf(stderr, "Can't use rules file '%s' - it contains rule longer than %d\n", argv[4], RULE_MAX_LENGTH);
+            return 1;
+        }
+
+        int len = strnlen(rule, sizeof(rule));
+        if (len > 1 && (rule[len - 2] == '\r' || rule[len - 2] == '\n'))
+            rule[len - 2] = 0;
+        if (len > 0 && (rule[len - 1] == '\r' || rule[len - 1] == '\n'))
+            rule[len - 1] = 0;
+
+        memcpy(all_rules + (RULE_MAX_LENGTH * rule_num++), rule, RULE_MAX_LENGTH);
+    }
+
+    fclose(rules_file);
+
+    if (dst_port <= 0 || dst_port > 65535) {
         fprintf(stderr, "Invalid dst_port '%s'\n", argv[3]);
         return 1;
     }
